bc68ed329c1462c0e2e862f7890d2bb3706b7f8a
siracoj <siraco.jared@gmail.com>
2014-05-25 20:50:04 -0400

    added shooting + explosions

--- ProPain/Movement.js
+++ ProPain/Movement.js
@@ -11,6 +11,8 @@ var GameState = function (game) {
 GameState.prototype.preload = function () {
     this.game.load.image('ground', '/assets/gfx/ground.png');
     this.game.load.image('player', '/assets/gfx/player.png');
+    this.game.load.spritesheet('explosion', '/assets/gfx/explosion.png', 128, 128);
+    this.game.load.image('bullet', '/assets/gfx/bullet.png');
 };
 
 // Setup the example
@@ -24,6 +26,9 @@ GameState.prototype.create = function () {
     this.DRAG = 600; // pixels/second
     this.GRAVITY = 2600; // pixels/second/second
     this.JUMP_SPEED = -700; // pixels/second (negative y is up)
+    this.SHOT_DELAY = 300; // milliseconds (10 bullets/3 seconds)
+    this.BULLET_SPEED = 800; // pixels/second
+    this.NUMBER_OF_BULLETS = 20;
 
     // Create a player sprite
     this.player = this.game.add.sprite(this.game.width / 2, this.game.height - 64, 'player');
@@ -59,6 +64,26 @@ GameState.prototype.create = function () {
         groundBlock.body.allowGravity = false;
         this.ground.add(groundBlock);
     }
+    
+    // Create an object pool of bullets
+    this.bulletPool = this.game.add.group();
+    for(var i = 0; i < this.NUMBER_OF_BULLETS; i++) {
+        // Create each bullet and add it to the group.
+        var bullet = this.game.add.sprite(0, 0, 'bullet');
+        this.bulletPool.add(bullet);
+
+        // Set its pivot point to the center of the bullet
+        bullet.anchor.setTo(0.5, 0.5);
+
+        // Enable physics on the bullet
+        this.game.physics.enable(bullet, Phaser.Physics.ARCADE);
+
+        // Set its initial state to "dead".
+        bullet.kill();
+    }
+    
+    // Create a group for explosions
+    this.explosionGroup = this.game.add.group();
 
     // Capture certain keys to prevent their default actions in the browser.
     // This is only necessary because this is an HTML5 game. Games on other
@@ -78,6 +103,81 @@ GameState.prototype.create = function () {
     this.fpsText = this.game.add.text(
         20, 20, '', { font: '16px Arial', fill: '#ffffff' }
     );
+};
+
+GameState.prototype.shootBullet = function() {
+    // Enforce a short delay between shots by recording
+    // the time that each bullet is shot and testing if
+    // the amount of time since the last shot is more than
+    // the required delay.
+    if (this.lastBulletShotAt === undefined) this.lastBulletShotAt = 0;
+    if (this.game.time.now - this.lastBulletShotAt < this.SHOT_DELAY) return;
+    this.lastBulletShotAt = this.game.time.now;
+
+    // Get a dead bullet from the pool
+    var bullet = this.bulletPool.getFirstDead();
+    this.game.physics.enable(bullet, Phaser.Physics.ARCADE);
+
+    // If there aren't any bullets available then don't shoot
+    if (bullet === null || bullet === undefined) return;
+
+    // Revive the bullet
+    // This makes the bullet "alive"
+    bullet.revive();
+
+    // Bullets should kill themselves when they leave the world.
+    // Phaser takes care of this for me by setting this flag
+    // but you can do it yourself by killing the bullet if
+    // its x,y coordinates are outside of the world.
+    bullet.checkWorldBounds = true;
+    bullet.outOfBoundsKill = true;
+    
+    //puts the bullet at the players position
+    bullet.reset(this.player.x, this.player.y);
+
+    // Set the bullet position to the gun position.
+    bullet.rotation = this.player.rotation;
+
+    // Shoot it in the right direction
+    bullet.body.velocity.x = Math.cos(bullet.rotation) * this.BULLET_SPEED;
+    bullet.body.velocity.y = Math.sin(bullet.rotation) * this.BULLET_SPEED;
+};
+
+GameState.prototype.getExplosion = function(x, y) {
+    // Get the first dead explosion from the explosionGroup
+    var explosion = this.explosionGroup.getFirstDead();
+
+    // If there aren't any available, create a new one
+    if (explosion === null) {
+        explosion = this.game.add.sprite(0, 0, 'explosion');
+        explosion.anchor.setTo(0.5, 0.5);
+
+        // Add an animation for the explosion that kills the sprite when the
+        // animation is complete
+        var animation = explosion.animations.add('boom', [0,1,2,3], 60, false);
+        animation.killOnComplete = true;
+
+        // Add the explosion sprite to the group
+        this.explosionGroup.add(explosion);
+    }
+
+    // Revive the explosion (set it's alive property to true)
+    // You can also define a onRevived event handler in your explosion objects
+    // to do stuff when they are revived.
+    explosion.revive();
+
+    // Move the explosion to the given coordinates
+    explosion.x = x;
+    explosion.y = y;
+
+    // Set rotation of the explosion at random for a little variety
+    explosion.angle = this.game.rnd.integerInRange(0, 360);
+
+    // Play the animation
+    explosion.animations.play('boom');
+
+    // Return the explosion itself in case we want to do anything else with it
+    return explosion;
 };
 
 // This function draws horizontal lines across the stage
@@ -115,6 +215,20 @@ GameState.prototype.update = function() {
     } else {
         this.player.body.acceleration.x = 0;
     }
+    
+    // Check if bullets have collided with the ground
+    this.game.physics.arcade.collide(this.bulletPool, this.ground, function(bullet, ground) {
+        // Create an explosion
+        this.getExplosion(bullet.x, bullet.y);
+
+        // Kill the bullet
+        bullet.kill();
+    }, null, this);
+
+    // Rotate all living bullets to match their trajectory
+    this.bulletPool.forEachAlive(function(bullet) {
+        bullet.rotation = Math.atan2(bullet.body.velocity.y, bullet.body.velocity.x);
+    }, this);
 
     // Set a variable that is true when the player is touching the ground
     var onTheGround = this.player.body.touching.down;
@@ -142,6 +256,9 @@ GameState.prototype.update = function() {
     if (!this.upInputIsActive()) {
         this.canVariableJump = false;
     }
+    if(this.input.keyboard.justPressed(Phaser.Keyboard.A)){
+        this.shootBullet();
+    }
 };
 
 // This function should return true when the player activates the "go left" control
@@ -156,6 +273,7 @@ GameState.prototype.leftInputIsActive = function() {
 
     return isActive;
 };
+
 
 // This function should return true when the player activates the "go right" control
 // In this case, either holding the right arrow or tapping or clicking on the right
