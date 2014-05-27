de4104da5c798970adbb0f86e4152d686cd2a2a5
siracoj <siraco.jared@gmail.com>
2014-05-27 19:30:48 -0400

    Made player its own object

--- ProPain/Engine.js
+++ ProPain/Engine.js
@@ -1,21 +1,18 @@
-// This example uses the Phaser 2.0.4 framework
-
-// Copyright © 2014 John Watson
-// Licensed under the terms of the MIT License
-/// <reference path="../phaser.js">
-
 var GameState = function (game) {
+    this.player = new Player(1, 'HANK');
 };
 
 // Load images and sounds
 GameState.prototype.preload = function () {
     this.game.load.image('ground', '/assets/gfx/ground.png');
-    this.game.load.image('player', '/assets/gfx/player.png');
+    
+    this.player.loadPlayer(this);
     this.game.load.spritesheet('explosion', '/assets/gfx/explosion.png', 128, 128);
     this.game.load.image('bullet', '/assets/gfx/bullet.png');
 };
 
-// Setup the example
+
+//
 GameState.prototype.create = function () {
     // Set stage background to something sky colored
     this.game.stage.backgroundColor = 0x4488cc;
@@ -30,30 +27,13 @@ GameState.prototype.create = function () {
     this.BULLET_SPEED = 800; // pixels/second
     this.NUMBER_OF_BULLETS = 20;
 
-    // Create a player sprite
-    this.player = this.game.add.sprite(this.game.width / 2, this.game.height - 64, 'player');
-
-    // Enable physics on the player
-    this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
-
-    // Make player collide with world boundaries so he doesn't leave the stage
-    this.player.body.collideWorldBounds = true;
-
-    // Set player minimum and maximum movement speed
-    this.player.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED * 10); // x, y
-
-    // Add drag to the player that slows them down when they are not accelerating
-    this.player.body.drag.setTo(this.DRAG, 0); // x, y
-
+    //Create Player
+    this.player.enablePlayer(this);
+    
     // Since we're jumping we need gravity
     this.game.physics.arcade.gravity.y = this.GRAVITY;
 
-    // Set a flag for tracking if we've double jumped
-    this.canDoubleJump = true;
-
-    // Set a flag for tracking if the player can adjust their jump height
-    this.canVariableJump = true;
-
+    
     // Create some ground for the player to walk on
     this.ground = this.game.add.group();
     for (var x = 0; x < this.game.width; x += 32) {
@@ -95,9 +75,6 @@ GameState.prototype.create = function () {
         Phaser.Keyboard.DOWN
     ]);
 
-    // Just for fun, draw some height markers so we can see how high we're jumping
-    this.drawHeightMarkers();
-
     // Show FPS
     this.game.time.advancedTiming = true;
     this.fpsText = this.game.add.text(
@@ -133,10 +110,10 @@ GameState.prototype.shootBullet = function() {
     bullet.outOfBoundsKill = true;
     
     //puts the bullet at the players position
-    bullet.reset(this.player.x, this.player.y);
+    bullet.reset(this.player.sprite.x, this.player.sprite.y);
 
     // Set the bullet position to the gun position.
-    bullet.rotation = this.player.rotation;
+    bullet.rotation = this.player.sprite.rotation;
 
     // Shoot it in the right direction
     bullet.body.velocity.x = Math.cos(bullet.rotation) * this.BULLET_SPEED;
@@ -203,18 +180,9 @@ GameState.prototype.update = function() {
         this.fpsText.setText(this.game.time.fps + ' FPS');
     }
 
-    // Collide the player with the ground
-    this.game.physics.arcade.collide(this.player, this.ground);
-
-    if (this.leftInputIsActive()) {
-        // If the LEFT key is down, set the player velocity to move left
-        this.player.body.acceleration.x = -this.ACCELERATION;
-    } else if (this.rightInputIsActive()) {
-        // If the RIGHT key is down, set the player velocity to move right
-        this.player.body.acceleration.x = this.ACCELERATION;
-    } else {
-        this.player.body.acceleration.x = 0;
-    }
+    //Update Player
+    this.player.movePlayer(this);
+    this.player.jumpPlayer(this);
     
     // Check if bullets have collided with the ground
     this.game.physics.arcade.collide(this.bulletPool, this.ground, function(bullet, ground) {
@@ -230,32 +198,7 @@ GameState.prototype.update = function() {
         bullet.rotation = Math.atan2(bullet.body.velocity.y, bullet.body.velocity.x);
     }, this);
 
-    // Set a variable that is true when the player is touching the ground
-    var onTheGround = this.player.body.touching.down;
-    if (onTheGround) this.canDoubleJump = true;
-
-    if (this.upInputIsActive(5)) {
-        // Allow the player to adjust his jump height by holding the jump button
-        if (this.canDoubleJump) this.canVariableJump = true;
-
-        if (this.canDoubleJump || onTheGround) {
-            // Jump when the player is touching the ground or they can double jump
-            this.player.body.velocity.y = this.JUMP_SPEED;
-
-            // Disable ability to double jump if the player is jumping in the air
-            if (!onTheGround) this.canDoubleJump = false;
-        }
-    }
-
-    // Keep y velocity constant while the jump button is held for up to 150 ms
-    if (this.canVariableJump && this.upInputIsActive(150)) {
-        this.player.body.velocity.y = this.JUMP_SPEED;
-    }
-
-    // Don't allow variable jump height after the jump button is released
-    if (!this.upInputIsActive()) {
-        this.canVariableJump = false;
-    }
+    
     if(this.input.keyboard.justPressed(Phaser.Keyboard.A)){
         this.shootBullet();
     }
---
+++ ProPain/Player.js
@@ -0,0 +1,87 @@
+//Player Class
+//Holds player object, and movement functions
+
+
+function Player(playerNumber, character){
+    
+    //////////////////////////////////////////Properties////////////////////////////////////////////////////
+    
+    //Player 1,2,...
+    this.playerNumber = playerNumber;  
+    //Chosen character
+    this.character = character;
+    // Set a flag for tracking if we've double jumped
+    this.canDoubleJump = true;
+    // Set a flag for tracking if the player can adjust their jump height
+    this.canVariableJump = true;
+    
+}   
+    ///////////////////////////////////////Player Functions////////////////////////////////////////////////
+   
+Player.prototype.loadPlayer = function(GameState){
+    GameState.game.load.image('player', '/assets/gfx/player.png');
+}
+    
+Player.prototype.enablePlayer = function(GameState){
+    //Load Player sprite
+    this.sprite = GameState.game.add.sprite(GameState.game.width / 2, GameState.game.height - 64, 'player');
+    
+    // Enable physics on the player
+    GameState.game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+
+    // Make player collide with world boundaries so he doesn't leave the stage
+    this.sprite.body.collideWorldBounds = true;
+
+    // Set player minimum and maximum movement speed
+    this.sprite.body.maxVelocity.setTo(GameState.MAX_SPEED, GameState.MAX_SPEED * 10); // x, y
+
+    // Add drag to the player that slows them down when they are not accelerating
+    this.sprite.body.drag.setTo(GameState.DRAG, 0); // x, y
+        
+}
+    
+Player.prototype.movePlayer = function(GameState){
+        
+    // Collide the player with the ground
+    GameState.game.physics.arcade.collide(this.sprite, GameState.ground);
+
+    if (GameState.leftInputIsActive()) {
+        // If the LEFT key is down, set the player velocity to move left
+        this.sprite.body.acceleration.x = -GameState.ACCELERATION;
+    } else if (GameState.rightInputIsActive()) {
+        // If the RIGHT key is down, set the player velocity to move right
+        this.sprite.body.acceleration.x = GameState.ACCELERATION;
+    } else {
+        this.sprite.body.acceleration.x = 0;
+    }
+        
+}
+    
+Player.prototype.jumpPlayer = function(GameState){
+    // Set a variable that is true when the player is touching the ground
+    var onTheGround = this.sprite.body.touching.down;
+    if (onTheGround) this.canDoubleJump = true;
+
+    if (GameState.upInputIsActive(5)) {
+        // Allow the player to adjust his jump height by holding the jump button
+        if (this.canDoubleJump) this.canVariableJump = true;
+
+        if (this.canDoubleJump || onTheGround) {
+            // Jump when the player is touching the ground or they can double jump
+            this.sprite.body.velocity.y = GameState.JUMP_SPEED;
+
+            // Disable ability to double jump if the player is jumping in the air
+            if (!onTheGround) this.canDoubleJump = false;
+        }
+    }
+
+    // Keep y velocity constant while the jump button is held for up to 150 ms
+    if (this.canVariableJump && GameState.upInputIsActive(150)) {
+        this.sprite.body.velocity.y = GameState.JUMP_SPEED;
+    }
+
+    // Don't allow variable jump height after the jump button is released
+    if (!GameState.upInputIsActive()) {
+        this.canVariableJump = false;
+    }
+}
