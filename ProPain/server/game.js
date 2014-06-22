d3b2c5c866be32eb61c52417f12ee584fe9ae27f
siracoj <siraco.jared@gmail.com>
2014-06-21 22:48:12 -0400

    game server

---
+++ ProPain/server/game.js
@@ -0,0 +1,13 @@
+//Place holder file for server operations
+
+var util = require("util"),
+    io = require("socket.io");
+
+var socket,
+    players;
+
+function init() {
+    players = [];
+};
+
+init();
