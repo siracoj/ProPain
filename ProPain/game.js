09b9671325cb0a738c428e60d09055060bbfd015
siracoj <siraco.jared@gmail.com>
2014-06-23 09:42:06 -0400

    Get rid of verbose option

--- ProPain/game.js
+++ ProPain/game.js
@@ -37,7 +37,7 @@ var socket,
         var file = req.params[0];
 
             //For debugging, we can track what files are requested.
-        if(verbose) console.log('\t :: Express :: file requested : ' + file);
+      console.log('\t :: Express :: file requested : ' + file);
 
             //Send the requesting client the file.
         res.sendfile( __dirname + '/' + file );
