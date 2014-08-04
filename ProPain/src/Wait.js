94770e08c4a8f0f3d3f219c848f8343b54d1822b
siracoj <siraco.jared@gmail.com>
2014-08-03 21:44:14 -0400

    brackets fix

---
+++ ProPain/src/Wait.js
@@ -0,0 +1,13 @@
+var WaitState = function(game){this.game = game};
+
+WaitState.prototype.preload = function(){
+
+};
+
+WaitState.prototype.create = function(){
+
+};
+
+WaitState.prototype.update = function(){
+
+};
